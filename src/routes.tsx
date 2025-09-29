1b4dc2c879185dd142b8e1202555517c08e2e9cb
Nourhan Adel <143334844+Nora-Shalaby@users.noreply.github.com>
2025-09-30 01:05:24 +0300

    add usermangement page

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,70 +3,75 @@ import Layout from "./components/layout/Layout";
 import DashboardLayout from "./dashboard/layout/DashboardLayout";
 import { Error, Home, NotFound, ProtectedRoute } from "./pages";
 import {
-    Dashboard,
-    UserManagement,
-    CourseManagement,
-    PaymentsRevenue,
-    PlatformSettings,
-    Reviews,
-    Reports,
-    DashboardError,
-    ProtectedAdminRoute,
+  Dashboard,
+  UserManagement,
+  CourseManagement,
+  PaymentsRevenue,
+  PlatformSettings,
+  Reviews,
+  Reports,
+  DashboardError,
+  ProtectedAdminRoute,
 } from "./dashboard/pages";
+import UserProfile from "./dashboard/pages/userManagement/UserProfile";
 
 export const router = createBrowserRouter([
-    { path: "*", element: <NotFound /> },
-    {
+  { path: "*", element: <NotFound /> },
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+    ),
+    errorElement: <Error />,
+    children: [
+      {
         path: "/",
-        element: (
-            <ProtectedRoute>
-                <Layout />
-            </ProtectedRoute>
-        ),
-        errorElement: <Error />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-        ],
-    },
-    {
-        element: (
-            <ProtectedAdminRoute>
-                <DashboardLayout />
-            </ProtectedAdminRoute>
-        ),
-        errorElement: <DashboardError />,
-        children: [
-            {
-                path: "/dashboard",
-                element: <Dashboard />,
-            },
-            {
-                path: "/user-management",
-                element: <UserManagement />,
-            },
-            {
-                path: "/course-management",
-                element: <CourseManagement />,
-            },
-            {
-                path: "/payments-revenue",
-                element: <PaymentsRevenue />,
-            },
-            {
-                path: "/platform-settings",
-                element: <PlatformSettings />,
-            },
-            {
-                path: "/reviews",
-                element: <Reviews />,
-            },
-            {
-                path: "/reports",
-                element: <Reports />,
-            },
-        ],
-    },
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    element: (
+      <ProtectedAdminRoute>
+        <DashboardLayout />
+      </ProtectedAdminRoute>
+    ),
+    errorElement: <DashboardError />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/user-management",
+        element: <UserManagement />,
+      },
+      {
+        path: "/course-management",
+        element: <CourseManagement />,
+      },
+      {
+        path: "/payments-revenue",
+        element: <PaymentsRevenue />,
+      },
+      {
+        path: "/platform-settings",
+        element: <PlatformSettings />,
+      },
+      {
+        path: "/reviews",
+        element: <Reviews />,
+      },
+      {
+        path: "/reports",
+        element: <Reports />,
+      },
+      {
+        path: "/user-profile/:id",
+        element: <UserProfile />,
+      },
+    ],
+  },
 ]);
