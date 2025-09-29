2c6762fb8bbfde7f80b4525b4157ad1219e63b53
YoussefTalaat60 <yousseftalaatbadawy123@gmail.com>
2025-09-29 15:31:10 +0300

    Save local changes in Home.tsx and package-lock.json

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,8 @@
+import Profile from "../Instractor/Profile/Profile"
+import MyCourses from "../Instractor/MyCourses/Courses"
 export default function Home() {
-  return <>Home</>;
+  return <>
+    <Profile />
+    <MyCourses />
+  </>;
 }
